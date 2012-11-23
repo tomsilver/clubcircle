0f512829dce15e8f2f0a354033570f55dff94100
Tom Silver <tomssilver@gmail.com>
2012-11-23 16:41:14 -0500

    First Upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Welcome to Club Circle</title>
+		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+		<link href='css/style.css' rel='stylesheet' type='text/css'/>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<div id="logo">
+			<h1>Club Circle</h1>
+		</div>
+		<div id="login">
+			<div id="fblogin">
+			</div>
+			<div id="huid">
+			</div>
+		</div>
+		<div id="login_search">
+			<table>
+				<tr>
+					<td>
+						<input style="width:280px;" id="search" name="search" type="text" placeholder="Search for clubs..." />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<div style="float: left;" class="login_button">Search</div>
+						<div style="float: right;" class="login_button">I'm Feeling Lucky</div>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
---
+++ main.php
@@ -0,0 +1,82 @@
+<html>
+	<head>
+		<title>Club Circle</title>
+		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+		<link href='css/style.css' rel='stylesheet' type='text/css'/>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<div id="leftbar">
+			<ul>
+				<li>Stuff</li>
+				<li>More Stuff</li>
+				<li>Your Stuff</li>
+				<li>Stuff Stuff</li>
+			<ul>
+		</div>
+		<div id="header">
+			<h2>Club Circle</h2>
+		</div>
+		<div id="advancedsearch">
+			<table>
+				<tr>
+					<td>
+						<input id="search" name="search" type="text" placeholder="Keywords" />
+					</td>
+					<td>
+						<input id="spec1" name="spec1" type="text" placeholder="Spec 1" />
+					</td>
+					<td>
+						<input id="spec2" name="spec2" type="text" placeholder="Spec 2" />
+					</td>
+				</tr>
+			</table>
+		</div>
+		<div id="results">
+			<table>
+				<tr style="background-color: #FFCCCC;" >
+					<th style="width: 10%;">Header 1</th>
+					<th style="width: 30%;">Header 2</th>
+					<th style="width: 45%;">Header 3</th>
+					<th style="width: 15%;">Header 4</th>
+					<th style="width: 10%;">Header 5</th>
+				</tr>
+				<tr style="background-color: #fff;">
+					<td>Result 1.1</td>
+					<td>Result 1.2</td>
+					<td>Result 1.3</td>
+					<td>Result 1.4</td>
+					<td>Result 1.5</td>
+				</tr>
+				<tr style="background-color: #FFFFCC;" >
+					<td>Result 2.1</td>
+					<td>Result 2.2</td>
+					<td>Result 2.3</td>
+					<td>Result 2.4</td>
+					<td>Result 2.5</td>
+				</tr>
+				<tr style="background-color: #fff;">
+					<td>Result 3.1</td>
+					<td>Result 3.2</td>
+					<td>Result 3.3</td>
+					<td>Result 3.4</td>
+					<td>Result 3.5</td>
+				</tr>
+				<tr style="background-color: #FFFFCC;">
+					<td>Result 4.1</td>
+					<td>Result 4.2</td>
+					<td>Result 4.3</td>
+					<td>Result 4.4</td>
+					<td>Result 4.5</td>
+				</tr>
+				<tr style="background-color: #fff;">
+					<td>Result 5.1</td>
+					<td>Result 5.2</td>
+					<td>Result 5.3</td>
+					<td>Result 5.4</td>
+					<td>Result 5.5</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
